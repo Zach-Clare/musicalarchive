be702670f3faa6872dbefb940a97fa02fff17ccb
Zach Clare2 <tclare50@gmail.com>
2022-11-22 17:17:55 +0000

    collapsable sections + rounded corners fix

--- web/js/collapsable_sections.js
+++ web/js/collapsable_sections.js
@@ -15,9 +15,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var infoboxes = document.getElementsByClassName("infobox__top");
     for (let i = 0; i < infoboxes.length; i++) {
         infoboxes[i].addEventListener("click", function(e) {
+            // collapse the section
             let infobox_top_proper = getParent("infobox__top", e.target);
-            let infobox_content = infobox_top_proper.nextElementSibling;
-            infobox_content.classList.toggle("d-none");
+            let infobox_content_wrapper = infobox_top_proper.nextElementSibling;
+            // infobox_content_wrapper.classList.toggle("d-none");
+
+            // rotate arrow
+            let chevron = e.target.querySelector(".fa-chevron-right");
+            // substitute one rotation class for the other
+            chevron.classList.toggle("fa-rotate-270"); 
+            chevron.classList.toggle("fa-rotate-90");
+
+            // transition sliding
+            if (infobox_content_wrapper.style.maxHeight !== "0px") {
+                infobox_content_wrapper.style.maxHeight = "0px";
+            } else {
+                infobox_content_wrapper.style.maxHeight = infobox_content_wrapper.scrollHeight + "px";
+            }
         });
     }
 });
